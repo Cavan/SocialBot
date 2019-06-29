31d636d8496e9af30087d82fbf936007ed6aaaa9
Cavan Biggs <cbiggs7635@conestogac.on.ca>
2019-06-29 00:55:34 -0400

    Initial instagram bot is working, able to login and follow a specific user

---
+++ commonImports.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.support import ui
+from selenium.webdriver.support import expected_conditions as EC 
+from selenium.webdriver.common.by import By
+import os
+import time
+import sys
--- instagramBot.py
+++ instagramBot.py
@@ -1,10 +1,4 @@
-from selenium import webdriver
-from selenium.webdriver.support import ui
-from selenium.webdriver.support import expected_conditions as EC 
-from selenium.webdriver.common.by import By
-import os
-import time
-import sys
+from commonImports import *
 
 class InstagramBot:
 
@@ -14,8 +8,6 @@ class InstagramBot:
         self.password = password
         self.base_url = 'https://www.instagram.com'
         self.driver = webdriver.Chrome('chromedriver.exe')
-
-      
 
         self.login()
         
@@ -31,16 +23,25 @@ class InstagramBot:
         ui.WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".aOOlW.HoLwm"))).click()
        
         #https://www.instagram.com/tonyrobbins/
-        time.sleep(15)
+        time.sleep(2)
 
     def nav_user(self, user):
         self.driver.get('{}/{}/'.format(self.base_url, user))
 
-    
+    #//*[@id="react-root"]/section/main/div/header/section/div[2]/div/span/span[1]/button
+
+    def follow_user(self, user):
+        self.nav_user(user)
+
+        follow_button = self.driver.find_elements_by_xpath("//button[contains(text(), 'Follow')]")[0]
+        #Delay for 1 second before clicking the button
+        time.sleep(1)
+        follow_button.click()
 
 
 if __name__ == '__main__':
     ig_bot = InstagramBot('kelsiketo@gmail.com','u00m840@#')
-    ig_bot.nav_user('tonyrobbins')
+    #ig_bot.nav_user('tonyrobbins')
+    ig_bot.follow_user('tonyrobbins')
     time.sleep(30)
     
